1f92b7489b0331e968647efada9a9e023b3348b9
Ejiroghene <ejiroghene15@gmail.com>
2019-09-24 13:37:22 +0100

    src files for ojet-frontend

--- src/js/appController.js
+++ src/js/appController.js
@@ -1,113 +1,134 @@
 /**
- * Copyright (c) 2014, 2017, Oracle and/or its affiliates.
+ * @license
+ * Copyright (c) 2014, 2019, Oracle and/or its affiliates.
  * The Universal Permissive License (UPL), Version 1.0
  */
 /*
  * Your application specific code will go here
  */
-define([
-  "ojs/ojcore",
-  "knockout",
-  "ojs/ojrouter",
-  "ojs/ojknockout",
-  "ojs/ojarraytabledatasource",
-  "ojs/ojoffcanvas"
-], function(oj, ko) {
-  function ControllerViewModel() {
-    var self = this;
+define(['knockout', 'ojs/ojmodule-element-utils', 'ojs/ojknockouttemplateutils', 'ojs/ojrouter', 'ojs/ojresponsiveutils', 'ojs/ojresponsiveknockoututils', 'ojs/ojarraydataprovider',
+  'ojs/ojoffcanvas', 'ojs/ojmodule-element', 'ojs/ojknockout'],
+  function (ko, moduleUtils, KnockoutTemplateUtils, Router, ResponsiveUtils, ResponsiveKnockoutUtils, ArrayDataProvider, OffcanvasUtils) {
+    function ControllerViewModel() {
+      var self = this;
 
-    // Media queries for repsonsive layouts
-    var smQuery = oj.ResponsiveUtils.getFrameworkQuery(
-      oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY
-    );
-    self.smScreen = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(
-      smQuery
-    );
-    var mdQuery = oj.ResponsiveUtils.getFrameworkQuery(
-      oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.MD_UP
-    );
-    self.mdScreen = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(
-      mdQuery
-    );
+      this.KnockoutTemplateUtils = KnockoutTemplateUtils;
 
-    //Global vars
-    self.isLoggedIn = ko.observable(false);
-    self.globalFullname = ko.observable();
-    self.globalUserid = ko.observable();
-    self.globalUsername = ko.observable();
-    self.globalPhone = ko.observable();
-    self.globalStack = ko.observable();
-    self.globalLocation = ko.observable();
-    self.globalEmail = ko.observable();
+      // Media queries for repsonsive layouts
+      var smQuery = ResponsiveUtils.getFrameworkQuery(ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY);
+      self.smScreen = ResponsiveKnockoutUtils.createMediaQueryObservable(smQuery);
+      var mdQuery = ResponsiveUtils.getFrameworkQuery(ResponsiveUtils.FRAMEWORK_QUERY_KEY.MD_UP);
+      self.mdScreen = ResponsiveKnockoutUtils.createMediaQueryObservable(mdQuery);
 
-    // Router setup
-    self.router = oj.Router.rootInstance;
-    self.router.configure({
-      signupPage: {
-        label: "Signup",
-        isDefault: true
-      },
-      loginPage: {
-        label: "Login"
-      },
-      dashboard: {
-        label: "Dashboard"
+      // Router setup
+      self.router = Router.rootInstance;
+      self.router.configure({
+        'dashboard': { label: 'Dashboard', },
+        'register': { label: 'Register' },
+        'login': { label: 'Login', isDefault: true },
+        'logout': { label: 'Logout' },
+      });
+      Router.defaults['urlAdapter'] = new Router.urlParamAdapter();
+
+      self.moduleConfig = ko.observable({ 'view': [], 'viewModel': null });
+
+      self.loadModule = function () {
+        ko.computed(function () {
+          var name = self.router.moduleConfig.name();
+          var viewPath = 'views/' + name + '.html';
+          var modelPath = 'viewModels/' + name;
+          var masterPromise = Promise.all([
+            moduleUtils.createView({ 'viewPath': viewPath }),
+            moduleUtils.createViewModel({ 'viewModelPath': modelPath })
+          ]);
+          masterPromise.then(
+            function (values) {
+              self.moduleConfig({ 'view': values[0], 'viewModel': values[1] });
+            }
+          );
+        });
+      };
+
+      // Navigation setup
+      var navData = [
+        {
+          name: 'Login', id: 'login', loggedIn: false
+        },
+        {
+          name: 'Register', id: 'register', loggedIn: false
+        },
+        {
+          name: 'Dashboard', id: 'dashboard', loggedIn: true
+        },
+        {
+          id: "logout", loggedIn: true, iconClass: "fa fa-power-off",
+        },
+      ];
+      self.navDataProvider = new ArrayDataProvider(navData, { keyAttributes: 'id' });
+
+      // Drawer
+      // Close offcanvas on medium and larger screens
+      self.mdScreen.subscribe(function () { OffcanvasUtils.close(self.drawerParams); });
+      self.drawerParams = {
+        displayMode: 'push',
+        selector: '#navDrawer',
+        content: '#pageContent'
+      };
+      // Called by navigation drawer toggle button and after selection of nav drawer item
+      self.toggleDrawer = function () {
+        return OffcanvasUtils.toggle(self.drawerParams);
       }
-    });
-    oj.Router.defaults["urlAdapter"] = new oj.Router.urlParamAdapter();
+      // Add a close listener so we can move focus back to the toggle button when the drawer closes
+      document.getElementById('navDrawer').addEventListener("ojclose", document.getElementById('drawerToggleButton').focus());
 
-    // Navigation setup
-    var navData = [
-      {
-        name: "Dashboard",
-        id: "dashboard",
-        iconClass:
-          "oj-navigationlist-item-icon demo-icon-font-24 demo-chart-icon-24"
+      // Header
+      // Application Name used in Branding Area
+      self.appName = ko.observable("OJET Team 20");
+      self.isLoggedIn = ko.observable(false);
+
+      // init database
+      var dbReq = window.indexedDB.open("ojetDb", 3);
+      (function initDb() {
+
+        if (!window.indexedDB) {
+          console.log("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
+        }
+        dbReq.onsuccess = function (event) {
+          db = dbReq.result;
+        };
+        dbReq.onupgradeneeded = function (event) {
+          // Save the IDBDatabase interface 
+          var db = event.target.result;
+          self.db = db;
+          // Create an objectStore for this database
+          let userTable
+          if (!db.objectStoreNames.contains('user')) {
+            userTable = db.createObjectStore("user", { keyPath: "id", autoIncrement: true });
+            userTable.createIndex("uname", "uname", { unique: true });
+            userTable.createIndex("email", "email", { unique: true });
+          }
+          userTable.transaction.oncomplete = function () {
+            console.log("database created successfully");
+          }
+        };
       }
-    ];
-    self.navDataSource = new oj.ArrayTableDataSource(navData, {
-      idAttribute: "id"
-    });
+      )();
 
-    // Drawer
-    // Close offcanvas on medium and larger screens
-    self.mdScreen.subscribe(function() {
-      oj.OffcanvasUtils.close(self.drawerParams);
-    });
-    self.drawerParams = {
-      displayMode: "push",
-      selector: "#navDrawer",
-      content: "#pageContent"
-    };
-    // Called by navigation drawer toggle button and after selection of nav drawer item
-    self.toggleDrawer = function() {
-      return oj.OffcanvasUtils.toggle(self.drawerParams);
-    };
-    // Add a close listener so we can move focus back to the toggle button when the drawer closes
-    $("#navDrawer").on("ojclose", function() {
-      $("#drawerToggleButton").focus();
-    });
+      // Footer
+      function footerLink(name, id, linkTarget) {
+        this.name = name;
+        this.linkId = id;
+        this.linkTarget = linkTarget;
+      }
+      self.footerLinks = ko.observableArray([
+        new footerLink('About Oracle', 'aboutOracle', 'http://www.oracle.com/us/corporate/index.html#menu-about'),
+        new footerLink('Contact Us', 'contactUs', 'http://www.oracle.com/us/corporate/contact/index.html'),
+        new footerLink('Legal Notices', 'legalNotices', 'http://www.oracle.com/us/legal/index.html'),
+        new footerLink('Terms Of Use', 'termsOfUse', 'http://www.oracle.com/us/legal/terms/index.html'),
+        new footerLink('Your Privacy Rights', 'yourPrivacyRights', 'http://www.oracle.com/us/legal/privacy/index.html')
+      ]);
+    }
 
-    // Header
-    // Application Name used in Branding Area
-    self.appName = ko.observable("OJET TEAM 7");
-    // User Info used in Global Navigation area
-    self.userLogin = ko.observable();
-
-    // Footer
-    // function footerLink(name, id, linkTarget) {
-    //     this.name = name;
-    //     this.linkId = id;
-    //     this.linkTarget = linkTarget;
-    // }
-    // self.footerLinks = ko.observableArray([
-    //     new footerLink('About Oracle', 'aboutOracle', 'http://www.oracle.com/us/corporate/index.html#menu-about'),
-    //     new footerLink('Contact Us', 'contactUs', 'http://www.oracle.com/us/corporate/contact/index.html'),
-    //     new footerLink('Legal Notices', 'legalNotices', 'http://www.oracle.com/us/legal/index.html'),
-    //     new footerLink('Terms Of Use', 'termsOfUse', 'http://www.oracle.com/us/legal/terms/index.html'),
-    //     new footerLink('Your Privacy Rights', 'yourPrivacyRights', 'http://www.oracle.com/us/legal/privacy/index.html')
-    // ]);
+    return new ControllerViewModel();
   }
-
-  return new ControllerViewModel();
-});
+);
--- src/js/viewModels/dashboard.js
+++ src/js/viewModels/dashboard.js
@@ -1,88 +1,104 @@
 /**
- * Copyright (c) 2014, 2017, Oracle and/or its affiliates.
+ * @license
+ * Copyright (c) 2014, 2019, Oracle and/or its affiliates.
  * The Universal Permissive License (UPL), Version 1.0
  */
 /*
  * Your dashboard ViewModel code goes here
  */
-define(["ojs/ojcore", "knockout", "jquery", "text!"], function(oj, ko, $) {
-  function DashboardViewModel() {
-    var self = this;
-    self.fullname = ko.observable();
-    self.userid = ko.observable();
-    self.email = ko.observable();
-    self.phone = ko.observable();
-    self.stack = ko.observableArray();
-    self.location = ko.observable();
+define([
+  'ojs/ojcore',
+  'knockout',
+  'ojs/ojrouter',
+  'jquery',
+  'ojs/ojlabel',
+  'ojs/ojbootstrap',
+  'ojs/ojselectcombobox',
+  'ojs/ojchart',
+  'ojs/ojlistview',
+  'ojs/ojarraydataprovider',
+],
+  function (oj, ko, Router) {
 
-    self.rootModel = ko.dataFor(document.getElementById("globalBody"));
+    function DashboardViewModel(params) {
+      let { rootInstance } = Router;
+      var self = this;
+      var rootViewModel = ko.dataFor(document.getElementById('globalBody'));
+      rootViewModel.isLoggedIn(true);
+      self.fullname = ko.observable();
+      self.username = ko.observable();
+      self.email = ko.observable();
+      self.phone = ko.observable();
+      self.stack = ko.observable();
+      self.location = ko.observable();
+      self.id = ko.observable();
 
-    self.fullname = self.rootModel.globalFullname;
-    self.userid = self.rootModel.globalUserid;
-    self.email = self.rootModel.globalEmail;
-    self.phone = self.rootModel.globalPhone;
-    self.stack = self.rootModel.globalStack;
-    self.location = self.rootModel.globalLocation;
-    // Below are a subset of the ViewModel methods invoked by the ojModule binding
-    // Please reference the ojModule jsDoc for additionaly available methods.
+      function loadData() {
+        let userData = localStorage.getItem("userInfo");
+        userData = JSON.parse(userData);
+        let { name, uname, email, phone, stack, location, id } = userData;
+        self.id(`hngi_${id}`);
+        self.fullname(name);
+        self.username(uname);
+        self.email(email);
+        self.phone(phone);
+        self.stack(stack);
+        self.location(location);
 
-    /**
-     * Optional ViewModel method invoked when this ViewModel is about to be
-     * used for the View transition.  The application can put data fetch logic
-     * here that can return a Promise which will delay the handleAttached function
-     * call below until the Promise is resolved.
-     * @param {Object} info - An object with the following key-value pairs:
-     * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-     * @param {Function} info.valueAccessor - The binding's value accessor.
-     * @return {Promise|undefined} - If the callback returns a Promise, the next phase (attaching DOM) will be delayed until
-     * the promise is resolved
-     */
-    self.handleActivated = function(info) {
-      // Implement if needed
-    };
+      }
+      // Below are a set of the ViewModel methods invoked by the oj-module component.
+      // Please reference the oj-module jsDoc for additional information.
 
-    /**
-     * Optional ViewModel method invoked after the View is inserted into the
-     * document DOM.  The application can put logic that requires the DOM being
-     * attached here.
-     * @param {Object} info - An object with the following key-value pairs:
-     * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-     * @param {Function} info.valueAccessor - The binding's value accessor.
-     * @param {boolean} info.fromCache - A boolean indicating whether the module was retrieved from cache.
-     */
-    self.handleAttached = function(info) {
-      // Implement if needed
-    };
+      /**
+       * Optional ViewModel method invoked after the View is inserted into the
+       * document DOM.  The application can put logic that requires the DOM being
+       * attached here.
+       * This method might be called multiple times - after the View is created
+       * and inserted into the DOM and after the View is reconnected
+       * after being disconnected.
+       */
+      self.connected = function () {
+        self.id('');
+        self.fullname('');
+        self.username('');
+        self.email('');
+        self.phone('');
+        self.stack('');
+        self.location('');
+        let u = localStorage.getItem("user");
+        if (u == null) {
+          rootInstance.go("login");
+        }
+        let userdata = db.transaction(["user"]).objectStore("user");
+        userdata.index('uname').get(`${u}`).onsuccess = function (e) {
+          let { name, uname, email, phone, stack, location, id } = e.target.result;
+          localStorage.setItem("userInfo", JSON.stringify({ name, uname, email, phone, stack, location, id }));
+          setTimeout(() => {
+            loadData();
+          }, 0);
+        }
+      };
 
-    /**
-     * Optional ViewModel method invoked after the bindings are applied on this View.
-     * If the current View is retrieved from cache, the bindings will not be re-applied
-     * and this callback will not be invoked.
-     * @param {Object} info - An object with the following key-value pairs:
-     * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-     * @param {Function} info.valueAccessor - The binding's value accessor.
-     */
-    self.handleBindingsApplied = function(info) {
-      // Implement if needed
-    };
+      /**
+       * Optional ViewModel method invoked after the View is disconnected from the DOM.
+       */
+      self.disconnected = function () {
+      };
+
+      /**
+       * Optional ViewModel method invoked after transition to the new View is complete.
+       * That includes any possible animation between the old and the new View.
+       */
+      self.transitionCompleted = function () {
+        // Implement if needed
+      };
+    }
 
     /*
-     * Optional ViewModel method invoked after the View is removed from the
-     * document DOM.
-     * @param {Object} info - An object with the following key-value pairs:
-     * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-     * @param {Function} info.valueAccessor - The binding's value accessor.
-     * @param {Array} info.cachedNodes - An Array containing cached nodes for the View if the cache is enabled.
+     * Returns a constructor for the ViewModel so that the ViewModel is constructed
+     * each time the view is displayed.  Return an instance of the ViewModel if
+     * only one instance of the ViewModel is needed.
      */
-    self.handleDetached = function(info) {
-      // Implement if needed
-    };
+    return new DashboardViewModel();
   }
-
-  /*
-   * Returns a constructor for the ViewModel so that the ViewModel is constrcuted
-   * each time the view is displayed.  Return an instance of the ViewModel if
-   * only one instance of the ViewModel is needed.
-   */
-  return new DashboardViewModel();
-});
+);
