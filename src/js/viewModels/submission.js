6911192f5f794deade4ff614eb5a3514d69950ac
KingAbesh <abeshekwere@gmail.com>
2019-09-30 23:54:02 +0100

    Submission Page

--- src/js/viewModels/submission.js
+++ src/js/viewModels/submission.js
@@ -7,34 +7,60 @@
  * Your incidents ViewModel code goes here
  */
 define([
-    "knockout",
-    "jquery",
-    "ojs/ojformlayout",
-    "ojs/ojselectcombobox",
-    "ojs/ojinputtext"
-  ], function(ko, $) {
-    function ExampleComponentModel(context) {
-      var self = this;
-      self.isSmall = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(
-        oj.ResponsiveUtils.getFrameworkQuery(
-          oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY
-        )
-      );
-      // For small screens: labels on top
-      // For medium or bigger: labels inline
-      self.labelEdge = ko.computed(function() {
-        return self.isSmall() ? "top" : "start";
-      }, self);
-  
-      // Form Input Observables
-      self.firstName = ko.observable("");
-      self.lastName = ko.observable("");
-      self.email = ko.observable("");
-      self.hngCode = ko.observable();
-      self.url = ko.observable();
-      self.taskDescription = ko.observable();
-    }
-  
-    return new ExampleComponentModel();
-  });
-  
+  "knockout",
+  "jquery",
+  "ojs/ojformlayout",
+  "ojs/ojselectcombobox",
+  "ojs/ojinputtext"
+], function(ko, $) {
+  function ExampleComponentModel(context) {
+    var self = this;
+    self.isSmall = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(
+      oj.ResponsiveUtils.getFrameworkQuery(
+        oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY
+      )
+    );
+    // For small screens: labels on top
+    // For medium or bigger: labels inline
+    self.labelEdge = ko.computed(function() {
+      return self.isSmall() ? "top" : "start";
+    }, self);
+
+    // Form Input Observables
+    self.firstName = ko.observable("");
+    self.lastName = ko.observable("");
+    self.email = ko.observable("");
+    self.hngCode = ko.observable("");
+    self.url = ko.observable("");
+    self.taskDescription = ko.observable("");
+
+    self.submit = () => {
+      // We do not have to prevent the form's default submission behaviour, knockout js enforces that as default behaviour. Pretty cool yeah !
+
+      //We need to check that the user does not submit empty fields;
+
+      if (
+        self.firstName() === "" ||
+        self.lastName() === "" ||
+        self.email() === "" ||
+        self.hngCode() === "" ||
+        self.url() === "" ||
+        self.taskDescription() === ""
+      ) {
+        return;
+      }
+      // Store the submitted input values in an object. This will be sent to the backend.
+      const task = {
+        firstname: self.firstName(),
+        lastname: self.lastName(),
+        email: self.email(),
+        hng_code: self.hngCode(),
+        url: self.url(),
+        task_description: self.taskDescription()
+      };
+      console.log(task);
+    };
+  }
+
+  return new ExampleComponentModel();
+});
