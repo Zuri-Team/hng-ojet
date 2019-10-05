0c772ef656caf3fffe20b6f27c6973f857524715
Ejiroghene <ejiroghene15@gmail.com>
2019-10-05 11:31:42 +0100

    app entry point

--- web/js/appController.js
+++ web/js/appController.js
@@ -1,1 +1,157 @@
-define(["knockout","ojs/ojmodule-element-utils","ojs/ojknockouttemplateutils","ojs/ojrouter","ojs/ojresponsiveutils","ojs/ojresponsiveknockoututils","ojs/ojarraydataprovider","ojs/ojoffcanvas","ojs/ojmodule-element","ojs/ojknockout"],function(e,o,r,a,t,s,l,i){return new function(){var n=this;this.KnockoutTemplateUtils=r;var u=t.getFrameworkQuery(t.FRAMEWORK_QUERY_KEY.SM_ONLY);n.smScreen=s.createMediaQueryObservable(u);var d=t.getFrameworkQuery(t.FRAMEWORK_QUERY_KEY.MD_UP);function c(e,o,r){this.name=e,this.linkId=o,this.linkTarget=r}n.mdScreen=s.createMediaQueryObservable(d),n.router=a.rootInstance,n.router.configure({dashboard:{label:"Dashboard"},admin_dashboard:{label:"Admin_dashboard"},register:{label:"Register"},submission:{label:"Submission"},login:{label:"Login",isDefault:!0},profile:{label:"User Profile"},password_reset:{label:"Reset password"}}),a.defaults.urlAdapter=new a.urlParamAdapter,n.moduleConfig=e.observable({view:[],viewModel:null}),n.loadModule=function(){e.computed(function(){var e=n.router.moduleConfig.name(),r="views/"+e+".html",a="viewModels/"+e;Promise.all([o.createView({viewPath:r}),o.createViewModel({viewModelPath:a})]).then(function(e){n.moduleConfig({view:e[0],viewModel:e[1]})})})},n.navDataProvider=new l([{name:"Login",id:"login"},{name:"Register",id:"register"},{name:"Dashboard",id:"dashboard"}],{keyAttributes:"id"}),n.mdScreen.subscribe(function(){i.close(n.drawerParams)}),n.drawerParams={displayMode:"push",selector:"#navDrawer",content:"#pageContent"},n.toggleDrawer=function(){return i.toggle(n.drawerParams)},n.appName=e.observable("OJET Team 20"),n.footerLinks=e.observableArray([new c("About Oracle","aboutOracle","http://www.oracle.com/us/corporate/index.html#menu-about"),new c("Contact Us","contactUs","http://www.oracle.com/us/corporate/contact/index.html"),new c("Legal Notices","legalNotices","http://www.oracle.com/us/legal/index.html"),new c("Terms Of Use","termsOfUse","http://www.oracle.com/us/legal/terms/index.html"),new c("Your Privacy Rights","yourPrivacyRights","http://www.oracle.com/us/legal/privacy/index.html")])}});
+/**
+ * @license
+ * Copyright (c) 2014, 2019, Oracle and/or its affiliates.
+ * The Universal Permissive License (UPL), Version 1.0
+ */
+/*
+ * Your application specific code will go here
+ */
+define([
+  "knockout",
+  "ojs/ojmodule-element-utils",
+  "ojs/ojknockouttemplateutils",
+  "ojs/ojrouter",
+  "ojs/ojresponsiveutils",
+  "ojs/ojresponsiveknockoututils",
+  "ojs/ojarraydataprovider",
+  "ojs/ojoffcanvas",
+  "ojs/ojmodule-element",
+  "ojs/ojknockout"
+], function(
+  ko,
+  moduleUtils,
+  KnockoutTemplateUtils,
+  Router,
+  ResponsiveUtils,
+  ResponsiveKnockoutUtils,
+  ArrayDataProvider,
+  OffcanvasUtils
+) {
+  function ControllerViewModel() {
+    var self = this;
+
+    this.KnockoutTemplateUtils = KnockoutTemplateUtils;
+
+    // Media queries for repsonsive layouts
+    var smQuery = ResponsiveUtils.getFrameworkQuery(
+      ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY
+    );
+    self.smScreen = ResponsiveKnockoutUtils.createMediaQueryObservable(smQuery);
+    var mdQuery = ResponsiveUtils.getFrameworkQuery(
+      ResponsiveUtils.FRAMEWORK_QUERY_KEY.MD_UP
+    );
+    self.mdScreen = ResponsiveKnockoutUtils.createMediaQueryObservable(mdQuery);
+
+    // Router setup
+    self.router = Router.rootInstance;
+    self.router.configure({
+      dashboard: { label: "Dashboard" },
+      admin_dashboard: {label: "Admin_dashboard"},
+      register: { label: "Register" },
+      submission: { label: "Submission" },
+      login: { label: "Login", isDefault: true},
+      profile: { label: "User Profile" },
+      password_reset: {label: "Reset password"}
+  
+    });
+    Router.defaults["urlAdapter"] = new Router.urlParamAdapter();
+
+    self.moduleConfig = ko.observable({ view: [], viewModel: null });
+
+    self.loadModule = function() {
+      ko.computed(function() {
+        var name = self.router.moduleConfig.name();
+        var viewPath = "views/" + name + ".html";
+        var modelPath = "viewModels/" + name;
+        var masterPromise = Promise.all([
+          moduleUtils.createView({ viewPath: viewPath }),
+          moduleUtils.createViewModel({ viewModelPath: modelPath })
+        ]);
+        masterPromise.then(function(values) {
+          self.moduleConfig({ view: values[0], viewModel: values[1] });
+        });
+      });
+    };
+
+    // Navigation setup
+    var navData = [
+      {
+        name: "Login",
+        id: "login"
+      },
+      {
+        name: "Register",
+        id: "register"
+      },
+      {
+        name: "Dashboard",
+        id: "dashboard"
+      }
+    ];
+    self.navDataProvider = new ArrayDataProvider(navData, {
+      keyAttributes: "id"
+    });
+
+    // Drawer
+    // Close offcanvas on medium and larger screens
+    self.mdScreen.subscribe(function() {
+      OffcanvasUtils.close(self.drawerParams);
+    });
+    self.drawerParams = {
+      displayMode: "push",
+      selector: "#navDrawer",
+      content: "#pageContent"
+    };
+    // Called by navigation drawer toggle button and after selection of nav drawer item
+    self.toggleDrawer = function() {
+      return OffcanvasUtils.toggle(self.drawerParams);
+    };
+    // Add a close listener so we can move focus back to the toggle button when the drawer closes
+    // document
+    //   .getElementById("navDrawer")
+    //   .addEventListener(
+    //     "ojclose",
+    //     document.getElementById("drawerToggleButton").focus()
+    //   );
+
+    // Header
+    // Application Name used in Branding Area
+    self.appName = ko.observable("OJET Team 20");
+
+    // Footer
+    function footerLink(name, id, linkTarget) {
+      this.name = name;
+      this.linkId = id;
+      this.linkTarget = linkTarget;
+    }
+    self.footerLinks = ko.observableArray([
+      new footerLink(
+        "About Oracle",
+        "aboutOracle",
+        "http://www.oracle.com/us/corporate/index.html#menu-about"
+      ),
+      new footerLink(
+        "Contact Us",
+        "contactUs",
+        "http://www.oracle.com/us/corporate/contact/index.html"
+      ),
+      new footerLink(
+        "Legal Notices",
+        "legalNotices",
+        "http://www.oracle.com/us/legal/index.html"
+      ),
+      new footerLink(
+        "Terms Of Use",
+        "termsOfUse",
+        "http://www.oracle.com/us/legal/terms/index.html"
+      ),
+      new footerLink(
+        "Your Privacy Rights",
+        "yourPrivacyRights",
+        "http://www.oracle.com/us/legal/privacy/index.html"
+      )
+    ]);
+  }
+
+  return new ControllerViewModel();
+});
