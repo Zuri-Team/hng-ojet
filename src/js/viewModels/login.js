a0d622bfcca3a9d8ff3f093069e21333324808c4
Ejiroghene <ejiroghene15@gmail.com>
2019-10-05 11:29:01 +0100

    login controller

--- src/js/viewModels/login.js
+++ src/js/viewModels/login.js
@@ -6,17 +6,21 @@
 /*
  * Your login ViewModel code goes here
  */
+
 define([
   "knockout",
+  "./api",
   "ojs/ojrouter",
   "ojs/ojresponsiveutils",
   "ojs/ojresponsiveknockoututils",
   "ojs/ojknockout",
   "ojs/ojinputtext",
   "ojs/ojbutton",
-  "ojs/ojformlayout"
-], function (ko, ResponsiveUtils, ResponsiveKnockoutUtils) {
+  "ojs/ojformlayout",
+], function (ko,api) {
+  
   function LoginViewModel() {
+
     var self = this;
     self.user = ko.observable();
     self.password = ko.observable();
@@ -52,7 +56,7 @@ define([
           sect.html(feedback("Please enter a valid email"));
         } else {
           sect.html(progressbar());
-          $.post("http://localhost:3000/api/login", {
+          $.post(`${api}/api/login`, {
             email,
             password
           })
@@ -70,32 +74,20 @@ define([
       }
     };
 
-    self.connected = function () { 
-      if(sessionStorage.getItem("user_token") !== null){
+    self.connected = function () {
+      if (sessionStorage.getItem("user_token") !== null) {
         router.go("dashboard");
-    }
+      }
     };
 
-    /**
-     * Optional ViewModel method invoked after the View is disconnected from the DOM.
-     */
     self.disconnected = function () {
       // Implement if needed
     };
 
-    /**
-     * Optional ViewModel method invoked after transition to the new View is complete.
-     * That includes any possible animation between the old and the new View.
-     */
     self.transitionCompleted = function () {
       // Implement if needed
     };
   }
 
-  /*
-   * Returns a constructor for the ViewModel so that the ViewModel is constructed
-   * each time the view is displayed.  Return an instance of the ViewModel if
-   * only one instance of the ViewModel is needed.
-   */
   return new LoginViewModel();
 });
