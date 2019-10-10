c9c3090233abd41bc2dbba32133c4d4145b0e9f1
Ejiroghene <ejiroghene15@gmail.com>
2019-10-10 08:47:46 +0100

    registration controller

--- src/js/viewModels/register.js
+++ src/js/viewModels/register.js
@@ -92,7 +92,7 @@ define([
 
           console.log(data)
             sect.html(progressbar());
-            $.post(`https://api.start.ng/api/register`, {
+            $.post(`${api}/api/register`, {
             firstname,
             lastname,
             email,
