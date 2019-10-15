4f3b16d24273ef08f937234788c53fdc9d3079ae
Ejiroghene <ejiroghene15@gmail.com>
2019-10-15 14:31:41 +0100

    apps entry point

--- src/js/appController.js
+++ src/js/appController.js
@@ -9,38 +9,12 @@
 define([
   "knockout",
   "ojs/ojmodule-element-utils",
-  "ojs/ojknockouttemplateutils",
   "ojs/ojrouter",
-  "ojs/ojresponsiveutils",
-  "ojs/ojresponsiveknockoututils",
-  "ojs/ojarraydataprovider",
-  "ojs/ojoffcanvas",
   "ojs/ojmodule-element",
   "ojs/ojknockout"
-], function(
-  ko,
-  moduleUtils,
-  KnockoutTemplateUtils,
-  Router,
-  ResponsiveUtils,
-  ResponsiveKnockoutUtils,
-  ArrayDataProvider,
-  OffcanvasUtils
-) {
+], function(ko, moduleUtils, Router) {
   function ControllerViewModel() {
     var self = this;
-
-    this.KnockoutTemplateUtils = KnockoutTemplateUtils;
-
-    // Media queries for repsonsive layouts
-    var smQuery = ResponsiveUtils.getFrameworkQuery(
-      ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY
-    );
-    self.smScreen = ResponsiveKnockoutUtils.createMediaQueryObservable(smQuery);
-    var mdQuery = ResponsiveUtils.getFrameworkQuery(
-      ResponsiveUtils.FRAMEWORK_QUERY_KEY.MD_UP
-    );
-    self.mdScreen = ResponsiveKnockoutUtils.createMediaQueryObservable(mdQuery);
 
     // Router setup
     self.router = Router.rootInstance;
@@ -48,15 +22,10 @@ define([
       dashboard: { label: "Dashboard" },
       admin_dashboard: { label: "Admin_dashboard" },
       user_dashboard: { label: "User_dashboard" },
-      overview: { label: "Overview" },
-      category: { label: "Category" },
       register: { label: "Register" },
-      submission: { label: "Submission" },
       login: { label: "Login", isDefault: true },
-      profile: { label: "User Profile" },
       password_reset: { label: "Reset password" },
-      tracks: { label: "Tracks" }
-  
+      submission: { label: "Submission" }
     });
     Router.defaults["urlAdapter"] = new Router.urlParamAdapter();
 
@@ -76,42 +45,6 @@ define([
         });
       });
     };
-
-    // Navigation setup
-    var navData = [
-      {
-        name: "Login",
-        id: "login"
-      },
-      {
-        name: "Register",
-        id: "register"
-      },
-      {
-        name: "Dashboard",
-        id: "dashboard"
-      }
-    ];
-    self.navDataProvider = new ArrayDataProvider(navData, {
-      keyAttributes: "id"
-    });
-
-    // Drawer
-    // Close offcanvas on medium and larger screens
-    self.mdScreen.subscribe(function() {
-      OffcanvasUtils.close(self.drawerParams);
-    });
-    self.drawerParams = {
-      displayMode: "push",
-      selector: "#navDrawer",
-      content: "#pageContent"
-    };
-    // Called by navigation drawer toggle button and after selection of nav drawer item
-    self.toggleDrawer = function() {
-      return OffcanvasUtils.toggle(self.drawerParams);
-    };
-
   }
-
   return new ControllerViewModel();
 });
