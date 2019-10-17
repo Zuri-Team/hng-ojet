2d46ee7102468dc8256c93ec8993cf1f88867d24
KingAbesh <abeshekwere@gmail.com>
2019-10-17 14:57:21 +0100

    Tracks CRUD - completed

--- src/js/viewModels/tracks.js
+++ src/js/viewModels/tracks.js
@@ -8,7 +8,8 @@ define([
   "ojs/ojbutton",
   "ojs/ojswitch",
   "ojs/ojdialog",
-  "ojs/ojinputtext"
+  "ojs/ojinputtext",
+  "ojs/ojmessages"
 ], function(ko, ArrayTreeDataProvider, api, keySet) {
   function tracksViewModel() {
     var self = this;
@@ -18,26 +19,30 @@ define([
     var userToken = sessionStorage.getItem("user_token");
 
     self.trackData = ko.observable(""); //holds data for the track details
-    self.newTrack = ko.observableArray([]); //newItem holds data for the create item dialog
+    self.newTrack = ko.observableArray([]); //newItem holds data for the create track dialog
     self.selectedItems = new keySet.ObservableKeySet(); // observable bound to selection option to monitor current selections
     self.selectedSelectionRequired = ko.observable(false);
     self.firstSelectedItem = ko.observable();
 
+    // notification messages observable
+    self.applicationMessages = ko.observableArray([]);
+
+    // Relevant observables
     self.selectedIds = ko.observableArray([]);
     self.currentItemId = ko.observable();
-
     self.dataProvider = ko.observable();
 
     self.handleSelectionChanged = function(event) {
       self.selectedIds(event.detail.value); // show selected list item elements' ids
 
       if (event.detail.value.length != 0) {
-        // If selection, populate and display Track details
-        // Populate items list observable using firstSelectedXxx API
+        // Populate tracks list observable using firstSelectedXxx API
         let { data } = self.firstSelectedItem();
         self.trackData(data);
       }
     };
+
+
 
     self.handleCurrentItemChanged = function(event) {
       var itemId = event.detail.value;
@@ -46,8 +51,9 @@ define([
       console.log(self.currentItemId());
     };
 
-    // Show dialogs
 
+
+    // Show dialogs
     self.showCreateTrack = function(event) {
       document.getElementById("createTrack").open();
     };
@@ -59,6 +65,8 @@ define([
       document.getElementById("deleteTrack").open();
     };
 
+
+    //  Fetch all tracks
     self.fetchTracks = async () => {
       try {
         const response = await fetch(`${tracksURL}/list`, {
@@ -80,10 +88,13 @@ define([
     };
     self.fetchTracks();
 
+
+    // Create tracks
     self.createTrack = async () => {
       let track_name = self.newTrack.track_name;
       let track_description = self.newTrack.track_description;
       console.log(track_name, track_description);
+
       try {
         const response = await fetch(`${tracksURL}/create`, {
           method: "POST",
@@ -96,24 +107,56 @@ define([
             track_description
           })
         });
+
+        // send a success message notification to the tracks view
+        self.applicationMessages.push({
+
+          severity: "confirmation",
+          summary: "New track created",
+          detail: "The new track " + track_name + " has been created"
+
+        });
+
         document.getElementById("createNewTitle").value = "";
         document.getElementById("createNewDesc").value = "";
         document.getElementById("createTrack").close();
         self.fetchTracks();
+
       } catch (err) {
+
+
         console.log(err);
+
+         // send an error message notification to the tracks view
+        self.applicationMessages.push({
+          severity: "error",
+          summary: "Error creating track",
+          detail: "Error trying to create new track"
+
+        });
+
       }
+
       console.log("track created");
+
     };
 
+
+    // edit tracks
     self.editTrack = async () => {
+
+      // This is plain es6 object destructuring. Sorry it is so nested.
       let {
         data: { id, track_description, track_name }
       } = self.firstSelectedItem();
+
       console.log(id, track_name, track_description);
-      let track_id = id;
+
+
+      let track_id = id;  // Form data needs id as track_id
 
       try {
+
         const response = await fetch(`${tracksURL}/edit`, {
           method: "PUT",
           headers: {
@@ -126,20 +169,41 @@ define([
             track_description
           })
         });
+
+      // send a success message notification to the tracks view
+        self.applicationMessages.push({
+          severity: "confirmation",
+          summary: track_name + " updated",
+          detail: "Track " + track_name + " has been updated"
+        });
         document.getElementById("editTrack").close();
         self.fetchTracks();
-      } catch (err) {
-        console.log(err);
-      }
-      console.log("track updated");
 
-      console.log("mama");
+      } catch (err) {
+
+        console.log(err);
+
+        // send an error message notification to the tracks view
+        self.applicationMessages.push({
+
+          severity: "error",
+          summary: "Error updating track",
+          detail: "Error trying to update track " + track_name
+
+        });
+
+      }
+
+      console.log("track updated");
     };
 
     self.deleteTrack = async () => {
+
       let track_id = self.currentItemId();
       console.log(self.currentItemId(), "yes");
+
       try {
+
         const response = await fetch(`${tracksURL}/delete`, {
           method: "DELETE",
           headers: {
@@ -149,11 +213,32 @@ define([
           body: JSON.stringify({
             track_id
           })
+
         });
+
+        // send a success message notification to the tracks view
+        self.applicationMessages.push({
+
+          severity: "confirmation",
+          summary: track_name + "deleted",
+          detail: "Track " + track_name + " has been deleted"
+
+        });
+
+
         document.getElementById("deleteTrack").close();
         self.fetchTracks();
+
       } catch (err) {
+
         console.log(err);
+
+        // send an error message notification to the tracks view
+        self.applicationMessages.push({
+          severity: "error",
+          summary: "Error deleting track",
+          detail: "Error trying to delete track " + track_name
+        });
       }
 
       console.log("papa");
