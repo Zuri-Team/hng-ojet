8b0627e3812c561efdeb167bc902863c7427c527
KingAbesh <abeshekwere@gmail.com>
2019-10-18 06:17:31 +0100

    necessary adjustments

--- src/js/viewModels/post.js
+++ src/js/viewModels/post.js
@@ -20,6 +20,9 @@ define([
     self.post_btn_toggler = ko.observable(false);
     self.post_view_title = ko.observable("New Post");
 
+    // notification messages observable
+    self.applicationMessages = ko.observableArray([]);
+
     self.post_view_toggle = function() {
       self.post_btn_toggler(!self.post_btn_toggler());
       self.post_view_title() == "New Post"
@@ -69,10 +72,28 @@ define([
         data: { category_id, post_title, post_body },
         success: res => {
           if (res.status == true) {
+            // send a success message notification to the category view
+            self.applicationMessages.push({
+              severity: "confirmation",
+              summary: "Post created",
+              detail: "A new post has been created",
+              autoTimeout: parseInt("0")
+            });
             reset();
             fetchposts();
             self.post_btn_toggler(!self.post_btn_toggler());
           }
+        },
+        error: err => {
+          console.log(err);
+
+          // send an error message notification to the category view
+          self.applicationMessages.push({
+            severity: "error",
+            summary: "Error creating post",
+            detail: "Error trying to create post",
+            autoTimeout: parseInt("0")
+          });
         }
       });
     };
