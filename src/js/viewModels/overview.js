d7f3ebfd237a6e2656cc673e2ec65476a60b4871
Gospel O. Chinyereugo <gospelokpara@gmail.com>
2019-10-23 16:47:05 +0100

    overview

--- src/js/viewModels/overview.js
+++ src/js/viewModels/overview.js
@@ -1,80 +1,126 @@
-    /**
- * @license
- * Copyright (c) 2014, 2019, Oracle and/or its affiliates.
- * The Universal Permissive License (UPL), Version 1.0
- */
-/*
- * Your login ViewModel code goes here
- */
-define(['knockout', 'ojs/ojbootstrap', 'ojs/ojvalidation-base','ojs/ojknockout', 'ojs/ojcomposite',
-'ojs/ojbutton','ojs/ojavatar','ojs/ojvalidation','ojs/ojlabel'],
-    function (ko, $, api, Bootstrap, ojvalbase, ArrayDataProvider) {
+define(['knockout', "jquery", "./api", 'ojs/ojbootstrap', 'ojs/ojarraydataprovider', 'ojs/ojpagingdataproviderview',  'ojs/ojpagingcontrol', 'ojs/ojknockout', 'ojs/ojtable'],
+function(ko, $, api, Bootstrap, ArrayDataProvider, PagingDataProviderView)
+{ 
+function overviewModel() {
+  var self = this;
+  self.interns = ko.observableArray([]);
 
-        function overviewModel() {
-            self = this;
+  self.id = ko.observable();
+  self.firstname = ko.observable();
+  self.lastname = ko.observable();
+  self.username = ko.observable();
+
+  self.overviewProvider = ko.observable();
+
+  var userToken = sessionStorage.getItem("user_token");
+
+  self.totalPosts = ko.observable('');
+            self.totalTeams = ko.observable('');
+            self.totalInterns = ko.observable('');
+            self.teamsProvider = ko.observable();
+            self.id = ko.observable();
+            self.team_name = ko.observable();
+            self.task = ko.observable();
+            self.status = ko.observable();
+            self.deadline = ko.observable();
+            self.team_lead = ko.observable();
+
+            var data = [
+                {
+                  "id": 0,
+                  "team_name": "Team OJET",
+                  "task": "Oracle JET",
+                  "team_lead": "Jude Jay",
+                  "deadline": "2019-10-07 14:18:34",
+                  "status": "Great"
+                },
+                {
+                  "id": 1,
+                  "team_name": "Team Lancer",
+                  "task": "Net worth calcultor",
+                  "team_lead": "Fabian Uzukwu",
+                  "deadline": "2019-10-07 12:48:32",
+                  "status": "Pending"
+                },
+                {
+                  "id": 2,
+                  "team_name": "Team Lancer",
+                  "task": "Net worth calcultor",
+                  "team_lead": "Fabian Uzukwu",
+                  "deadline": "2019-10-07 12:48:32",
+                  "status": "Pending"
+                },
+                {
+                  "id": 3,
+                  "team_name": "Team Lancer",
+                  "task": "Net worth calcultor",
+                  "team_lead": "Fabian Uzukwu",
+                  "deadline": "2019-10-07 12:48:32",
+                  "status": "Pending"
+                },
+                {
+                  "id": 4,
+                  "team_name": "Team Lancer",
+                  "task": "Net worth calcultor",
+                  "team_lead": "Fabian Uzukwu",
+                  "deadline": "2019-10-07 12:48:32",
+                  "status": "Pending"
+                },
+                {
+                  "id": 5,
+                  "team_name": "Team Lancer",
+                  "task": "Net worth calcultor",
+                  "team_lead": "Fabian Uzukwu",
+                  "deadline": "2019-10-07 12:48:32",
+                  "status": "Pending"
+                },
+                {
+                  "id": 6,
+                  "team_name": "Team Lancer",
+                  "task": "Net worth calcultor",
+                  "team_lead": "Fabian Uzukwu",
+                  "deadline": "2019-10-07 12:48:32",
+                  "status": "Pending"
+                }
+              ];
+
            
-
-            var RESTurl = 'https://api.start.ng/api/stats/dashboard';
-
-            self.total_posts = ko.observable();
-            self.total_teams = ko.observable();
-            self.total_interns = ko.observabl();
-           
-            self.fetchdashboard = function() {
+            function fetchdashboard () {
               $.ajax({
-                url: `${RESTurl}`,
+                url: `${api}/api/stats/dashboard`,
                 headers:{
-                  Authorization: "Bearer" + userToken
+                  Authorization: "Bearer " + userToken
                 },
                 method: "GET",
-                success: res =>{
-                  let{data} = res.data;
-                  self.total_posts(data.total_posts);
-                  self.total_teams(data.total_teams);
-                  self.total_interns(data.total_interns);
+                success: ({status, data}) => {
+                    if (status == true) {
+                        self.totalPosts(data.total_posts);
+                        self.totalTeams(data.total_teams);
+                        self.totalInterns(data.total_interns);
+                    }
                 }
-
-              })
+              });  
             }
-            
 
-            /*self.dataProvider = ko.observable();
-          
-            var userToken = sessionStorage.getItem("user_token");
+    fetchdashboard();
 
-    fetch('https://api.start.ng/api/stats/dashboard')
-    .then(res => res.json())
-    .then(data => console.log(data));
-      */     
-    
-    
-            self.firstName = '';
-            self.lastName = '';
-            self.initials = ojvalbase.IntlConverterUtils.getInitials(self.firstName,self.lastName);
-            self.avatarSize = ko.observable("xxl");
-            self.sizeOptions = ko.observableArray(['xxs', 'xs','sm','md','lg','xl','xxl']);
-            
-            /**
-             * Optional ViewModel method invoked after the View is disconnected from the DOM.
-             */
-            /*self.disconnected = function () {
-                // Implement if needed
-            };
+//   function fetchoverview() {
+//     $.ajax({
+//       url: `${api}/api/interns`,
+//       headers: {
+//         Authorization: "Bearer " + userToken
+//       },
+//       method: "GET",
+//       success: ({status, data}) => {
+//         if (status == true) {
+//           self.overviewProvider(new PagingDataProviderView(new ArrayDataProvider(data, {keyAttributes: 'id'})));
+//       }
+//     }
+//   });  
+// }
+// fetchoverview();
 
-            /**
-             * Optional ViewModel method invoked after transition to the new View is complete.
-             * That includes any possible animation between the old and the new View.
-             */
-           /* self.transitionCompleted = function () {
-                // Implement if needed
-            };
-        }
-
-        /*
-         * Returns a constructor for the ViewModel so that the ViewModel is constructed
-         * each time the view is displayed.  Return an instance of the ViewModel if
-         * only one instance of the ViewModel is needed.
-         */
-        return new overviewModel();
-    });
-
+self.overviewProvider(new PagingDataProviderView(new ArrayDataProvider(data, {keyAttributes: 'id'})));
+}
+return new overviewModel();
+});
