c7d9d7a2d69a60bbca8485123d872366e8feb504
jiroGhene <31715601+jiroGhene@users.noreply.github.com>
2019-10-25 10:49:09 +0100

    fixed summary chart display

--- src/js/viewModels/summary.js
+++ src/js/viewModels/summary.js
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-define(['knockout', "jquery", "./api", 'ojs/ojbootstrap','ojs/ojcomponentcore', 'ojs/ojarraydataprovider', 'ojs/ojchart', 'ojs/ojknockout'],
-function(ko, $, api, Bootstrap, Components, ArrayDataProvider)
-{   
-function summaryModel() {
-
-  var self = this;
-  self.chartProvider = ko.observable();
-  self.trackProvider = ko.observable();
-  self.stageProvider = ko.observable();
-
-/* chart data */
-
-
-var userToken = sessionStorage.getItem("user_token");
-
-
-           
-function fetchsummary () {
-  $.ajax({
-    url: `${api}/api/stats/summary`,
-    headers:{
-      Authorization: "Bearer " + userToken
-    },
-    wait: true,
-    method: "GET",
-    success: ({status, data}) => {
-      if (status == "success") {
-        // console.log(data);
-        self.chartProvider(new ArrayDataProvider(data.gender, {keyAttributes: 'id'}));
-        // console.log(self.chartProvider());
-        self.stageProvider(new ArrayDataProvider(data.stages, {keyAttributes: 'id'}));
-        self.trackProvider(new ArrayDataProvider(data.tracks, {keyAttributes: 'id'}));
-        // self.dataProvider(new PagingDataProviderView(new ArrayDataProvider(data, {keyAttributes: 'id'})));
-    }
-  },
-    error: (err) => {
-      console.log(err)
-    }
-  });  
-}
-fetchsummary();
-
-// subtreeShown(node, true)
-};
-=======
 define([
   "knockout",
   "jquery",
@@ -96,13 +50,17 @@ define([
 
     fetchsummary();
 
-    self.connected = function() {
-      //new
 
-      // Components.subtreeAttached(document.getElementById("summary-admin"));
-    };
+    let pm = ko.dataFor(document.querySelector("#admin"));
+    pm.selectedItem.subscribe(function() {
+      if (pm.selectedItem() == "Summary") {
+
+      Components.subtreeShown(document.getElementById('summary-admin'))
+
+      }
+    });
+
   }
->>>>>>> 9590cb650df9684c70eccb27ff74e1cddf20b135
 
   return new summaryModel();
 });
