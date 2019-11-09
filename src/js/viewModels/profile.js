f10373cadb50f4c24d39011ee0a186f1a8d6f448
Ejiroghene <ejiroghene15@gmail.com>
2019-11-09 08:43:00 +0100

    refactored intern profile page

--- src/js/viewModels/profile.js
+++ src/js/viewModels/profile.js
@@ -1,18 +1,13 @@
 define(['ojs/ojcore',
-        'knockout',
-        'jquery', './api',
-        'ojs/ojbootstrap',
-        'ojs/ojresponsiveutils',
-        'ojs/ojresponsiveknockoututils', 'ojs/ojarraydataprovider','ojs/ojanimation', 'ojs/ojmessages',
-        'ojs/ojknockout', 'ojs/ojlabel', 'ojs/ojdialog', 'ojs/ojavatar', 'ojs/ojselectcombobox',
-        'ojs/ojfilepicker', 'ojs/ojinputtext', 'ojs/ojformlayout', 'ojs/ojbutton'
-    ],
-    function(oj, ko, $, api, ArrayDataProvider, ) {
+  'knockout',
+  'jquery',
+  './api',
+  'ojs/ojmessages'
+],
+        function (oj, ko, $, api, ArrayDataProvider, ) {
 
-        function ProfileViewModel() {
+          function ProfileViewModel() {
             var self = this;
-
-            // 
             var RESTurl = `${api}/api/profile`;
             var userToken = sessionStorage.getItem("user_token");
             const user = JSON.parse(sessionStorage.getItem("user"));
@@ -20,8 +15,6 @@ define(['ojs/ojcore',
 
             // notification messages observable
             self.applicationMessages = ko.observableArray([]);
-
-
             self.firstname = ko.observable('');
             self.lastname = ko.observable('');
             self.username = ko.observable('@');
@@ -31,255 +24,148 @@ define(['ojs/ojcore',
             self.stack = ko.observable('');
             self.url = ko.observable('');
             self.location = ko.observable('');
-
-
-            self.editMode = ko.observable(false);
-
             self.profile = ko.observable('');
-
-            self.status = ko.observable(false);
-
             self.devstack = ko.observableArray([
-                { value: 'UI/UX', label: 'UI/UX' },
-                { value: 'FrontEnd', label: 'FrontEnd' },
-                { value: 'Backend', label: 'Backend' },
-                { value: 'Digital Marketing', label: 'Digital Marketing' },
-                { value: 'DevOps', label: 'DevOps' },
-                { value: 'FrontEnd', label: 'FrontEnd' },
+              {value: 'UI/UX', label: 'UI/UX'},
+              {value: 'FrontEnd', label: 'FrontEnd'},
+              {value: 'Backend', label: 'Backend'},
+              {value: 'Digital Marketing', label: 'Digital Marketing'},
+              {value: 'DevOps', label: 'DevOps'},
+              {value: 'FrontEnd', label: 'FrontEnd'},
             ]);
-
             //Events
             self.acceptStr = ko.observable("image/*");
-
-            self.acceptArr = ko.pureComputed(function() {
-                var accept = self.acceptStr();
-                return accept ? accept.split(",") : [];
+            self.acceptArr = ko.pureComputed(function () {
+              var accept = self.acceptStr();
+              return accept ? accept.split(",") : [];
             }, self);
+            self.editing = ko.observable(false);
+            self.editprofile = () => {
+              self.editing(true);
+            };
+            self.cancelediting = () => {
+              self.editing(false);
+            };
+            self.update = () => {
+              console.log("updating...");
+              const form = {
+                firstname: self.firstname(),
+                lastname: self.lastname(),
+                username: self.username(),
+                email: self.email(),
+                bio: self.bio(),
+                url: self.url()
+              }
+              $.ajax({
+                url: `${RESTurl}/${id}/edit`,
+                headers: {
+                  Accept: 'application/json',
+                  Authorization: "Bearer " + userToken
+                },
+                data: form,
+                contentType: 'application/x-www-form-urlencoded',
+                method: 'POST',
+                type: 'POST',
+                success: function ( {status}) {
+                  if (status == true) {
+                    self.editing(false);
+                    self.fetchProfile();
+                    self.applicationMessages.push({
 
+                      severity: "confirmation",
+                      summary: "Update Successful",
+                      detail: "Your profile has been successfully updated",
+                      autoTimeout: parseInt("0")
 
-            self.selectListener = function(event) {
-                const file = event.detail.files[0];
-
-                let form = new FormData();
-
-                form.append('profile_img', file);
-
-                $.ajax({
-                    url: `${RESTurl}/${id}/upload`,
-                    headers: {
-                        Accept: 'application/json',
-                        Authorization: "Bearer " + userToken
-                    },
-                    data: form,
-                    contentType: false,
-                    processData: false,
-                    method: 'POST',
-                    type: 'POST',
-                    success: function(data) {
-                        self.fetchProfile();
-                        self.applicationMessages.push({
-                            
-                            severity: "confirmation",
-                            summary: "Avatar Updated",
-                            detail: "Click on The Update Button To Proceed",
-                            autoTimeout: parseInt("0")
-
-                        });
-                    },
-                    error: function(error) {
-                       
-                        self.applicationMessages.push({
-                            severity: "error",
-                            summary: "Failed to Update",
-                            detail: "An error occurred while updating your avatar. Try Again",
-                            autoTimeout: parseInt("0")
-
-                        });
-
-                    }
-                });
-
-
-            }
-            
-
-            self.showEditDialog = function (event) {
-                document.getElementById("editModalDialog").open();
-                console.log('clicked')
-            }
-            
-            self.editButton = function() {
-
-                self.editMode() ?
-                    self.editMode(false) :
-                    self.editMode(true);
-
-                self.fetchProfile();
-
-            }.bind(self);
-
-            self.cancelButton = function() {
-
-                self.editMode() ?
-                    self.editMode(false) :
-                    self.editMode(true);
-
-                self.fetchProfile();
-                
-
-            }.bind(self);
-
-            self.update = function(data, event) {
-
-                event.preventDefault();
-
-                const {...user } = self.profile;
-
-
-
-                const form = {
-
-                    firstname: user.firstname,
-                    lastname: user.lastname,
-                    username: user.username,
-                    email: user.email,
-                    bio: user.bio,
-                    url: user.url,
-
+                    });
                 }
+                },
+                error: function (error) {
+                  self.applicationMessages.push({
+                    severity: "error",
+                    summary: "Failed to Update",
+                    detail: "An error occurred while updating your profile. Try Again",
+                    autoTimeout: parseInt("0")
 
-                $.ajax({
-                    url: `${RESTurl}/${id}/edit`,
-                    headers: {
-                        Accept: 'application/json',
-                        Authorization: "Bearer " + userToken
-                    },
-                    data: form,
-                    contentType: 'application/x-www-form-urlencoded',
-                    method: 'POST',
-                    type: 'POST',
-                    success: function(data) {
-                        self.fetchProfile();
-                        self.editMode(false)
-                        self.applicationMessages.push({
-
-                            severity: "confirmation",
-                            summary: "Update Successful",
-                            detail: "Your profile has been successfully updated",
-                            autoTimeout: parseInt("0")
-
-                        });
-                    },
-                    error: function(error) {
-                        self.editMode(true)
-                        self.applicationMessages.push({
-                            severity: "error",
-                            summary: "Failed to Update",
-                            detail: "An error occurred while updating your profile. Try Again",
-                            autoTimeout: parseInt("0")
-
-                        });
-                    }
-                });
-
-
-
-            }
-
-            self.fetchProfile = function() {
-
-                $.ajax({
-                    url: `${RESTurl}/${id}`,
-                    headers: {
-                        Authorization: "Bearer " + userToken
-                    },
-                    method: "GET",
-                    success: res => {
-
-
-                        const [...data] = res.data
-                        const [user, profile] = data
-
-                        const { firstname, lastname, username, email, } = user;
-                        const { bio, url, phone, profile_img } = profile;
-
-                        self.firstname(firstname);
-                        self.lastname(lastname);
-                        self.username(username);
-                        self.email(email);
-                        self.bio(bio);
-                        self.url(url);
-                        // self.phone(phone)
-
-                        let profileImg = document.getElementsByClassName('profile_img');
-
-                        for (i = 0; i < profileImg.length; i++) {
-                            profileImg[i].src = profile_img;
-                        }
-
-                    }
-                });
-
+                  });
+                }
+              });
             };
 
-            (function fetchUser() {
-                $.ajax({
-                    url: `${api}/api/status`,
-                    method: "GET",
-                    success: ({ status, data }) => {
-                        if (status == true) {
-                            const [res] = data.filter(data => data.id === id);
+            self.fetchProfile = function () {
 
-                            self.status(res.status);
-                        }
+              $.ajax({
+                url: `${RESTurl}/${id}`,
+                headers: {
+                  Authorization: "Bearer " + userToken
+                },
+                method: "GET",
+                success: res => {
+                  const [...data] = res.data
+                  const [user, profile] = data
+                  const {firstname, lastname, username, email, } = user;
+                  const {bio, url, phone, profile_img} = profile;
+                  self.firstname(firstname);
+                  self.lastname(lastname);
+                  self.username(username);
+                  self.email(email);
+                  self.bio(bio);
+                  self.url(url);
+                  // self.phone(phone)
 
-
-                    }
-                });
-                setTimeout(fetchUser, 15000);
-            })();
-
+                  let profileImg = document.getElementsByClassName('profile_img');
+                  for (i = 0; i < profileImg.length; i++) {
+                    profileImg[i].src = profile_img;
+                  }
+                }
+              });
+            };
             self.fetchProfile();
-
-
-         
-
-            /**
-             * Optional ViewModel method invoked after the View is inserted into the
-             * document DOM.  The application can put logic that requires the DOM being
-             * attached here.
-             * This method might be called multiple times - after the View is created
-             * and inserted into the DOM and after the View is reconnected
-             * after being disconnected.
-             */
-            self.connected = function() {
-                // Implement if needed
-
-
+            self.handleAttached = () => {
+              self.select_file = function () {
+                $("#profilephoto").trigger("click");
+              };
+              $("#profilephoto").on("change", () => {
+                let file = document.querySelector("#profilephoto").files[0];
+                let form = new FormData();
+                form.append('profile_img', file);
+                const reader = new FileReader();
+                reader.onload = function (e) {
+                  $(".profile_img").attr("src", reader.result);
+                };
+                reader.readAsDataURL(file);
+                $.ajax({
+                  url: `${RESTurl}/${id}/upload`,
+                  headers: {
+                    Accept: 'application/json',
+                    Authorization: "Bearer " + userToken
+                  },
+                  data: form,
+                  contentType: false,
+                  processData: false,
+                  method: 'POST',
+                  success: function (data) {
+//                    self.fetchProfile();
+                    self.applicationMessages.push({
+                      severity: "confirmation",
+                      summary: "Avatar Updated",
+                      autoTimeout: parseInt("0")
+                    });
+                  },
+                  error: function (error) {
+                    self.applicationMessages.push({
+                      severity: "error",
+                      summary: "Failed to Update",
+                      detail: "An error occurred while updating your avatar. Try Again",
+                      autoTimeout: parseInt("0")
+                    });
+                  }
+                });
+              });
             };
+          }
 
-            /**
-             * Optional ViewModel method invoked after the View is disconnected from the DOM.
-             */
-            self.disconnected = function() {
-                // Implement if needed
-            };
-
-            /**
-             * Optional ViewModel method invoked after transition to the new View is complete.
-             * That includes any possible animation between the old and the new View.
-             */
-            self.transitionCompleted = function() {
-                // Implement if needed
-            };
+          return new ProfileViewModel();
         }
-
-        /*
-         * Returns a constructor for the ViewModel so that the ViewModel is constructed
-         * each time the view is displayed.  Return an instance of the ViewModel if
-         * only one instance of the ViewModel is needed.
-         */
-        return new ProfileViewModel();
-    }
 
 );
