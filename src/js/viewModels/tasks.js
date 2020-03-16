d412d4ca4ad39543c5bc977b060b065e1942e2c0
KingAbesh <abeshekwere@gmail.com>
2020-03-16 13:07:10 +0100

    taking out console.logs

--- src/js/viewModels/tasks.js
+++ src/js/viewModels/tasks.js
@@ -217,8 +217,6 @@ define([
       let deadline = self.newTask().deadline;
       let is_active = self.newTask().is_active;
 
-      console.log(track_id, title, body, deadline, is_active);
-
       $.ajax({
         method: "POST",
         url: `${tasksURL}`,
